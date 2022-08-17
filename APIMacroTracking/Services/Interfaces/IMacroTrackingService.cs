37220285cc448f6bf85200546345c9ac68fda652
nvaughan00 <46831906+nvaughan00@users.noreply.github.com>
2022-08-16 20:00:31 -0500

    Add files via upload

---
+++ APIMacroTracking/Services/Interfaces/IMacroTrackingService.cs
@@ -0,0 +1,13 @@
+﻿using APIMacroTracking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIMacroTracking.Services
+{
+    public interface IMacroTrackingService
+    {
+        List<Item> GetAllItems();
+    }
+}

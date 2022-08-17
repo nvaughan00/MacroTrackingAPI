393af0cadfffec9efcf62077297d1bd8b6cf0fe3
nvaughan00 <46831906+nvaughan00@users.noreply.github.com>
2022-08-16 20:00:58 -0500

    Add files via upload

---
+++ APIMacroTracking/Services/Implementations/MacroTrackingService.cs
@@ -0,0 +1,27 @@
+﻿using APIMacroTracking.Contexts;
+using APIMacroTracking.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIMacroTracking.Services
+{
+    public class MacroTrackingService : IMacroTrackingService
+    {
+        protected readonly IConfiguration config;
+        protected readonly MacroTrackingDBContext context;
+
+        public MacroTrackingService(IConfiguration config, MacroTrackingDBContext context)
+        {
+            this.config = config;
+            this.context = context;
+        }
+
+        public List<Item> GetAllItems()
+        {
+            return context.Items.ToList();
+        }
+    }
+}

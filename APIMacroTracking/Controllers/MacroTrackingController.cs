f89e275b6aea6d1e2fc3b4675fbebc12453d5d21
nvaughan00 <46831906+nvaughan00@users.noreply.github.com>
2022-08-16 19:36:22 -0500

    Add files via upload

---
+++ APIMacroTracking/Controllers/MacroTrackingController.cs
@@ -0,0 +1,29 @@
+﻿using APIMacroTracking.Models;
+using APIMacroTracking.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIMacroTracking.Controllers
+{
+    [ApiController]
+    [Route("TEST")]
+    public class MacroTrackingController : ControllerBase
+    {
+        private readonly IMacroTrackingService service;
+
+        public MacroTrackingController(IMacroTrackingService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet("Testing")]
+        public IEnumerable<Item> Get()
+        {
+            return service.GetAllItems();
+        }
+    }
+}

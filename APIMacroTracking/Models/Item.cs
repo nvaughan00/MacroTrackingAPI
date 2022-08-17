f34e77f5dbb7e9df0e88aa4d80fa71cbc98b1c10
nvaughan00 <46831906+nvaughan00@users.noreply.github.com>
2022-08-16 19:59:33 -0500

    Add files via upload

---
+++ APIMacroTracking/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIMacroTracking.Models
+{
+    public class Item
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Calories { get; set; }
+        public decimal Protein { get; set; }
+        public decimal Carbs { get; set; }
+        public decimal Fat { get; set; }
+        public string FoodType { get; set; }
+    }
+}
